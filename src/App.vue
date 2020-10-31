aa016fce045f721e6608636d7bf55f971132fa4e
DoLevi <domi.genz@gmail.com>
2020-10-31 21:32:27 +0100

    Stripped default elements

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <layout />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
+import Layout from "./components/Layout.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld
+    Layout
   }
 });
 </script>
@@ -22,6 +22,5 @@ export default defineComponent({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        Helo
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Layout"
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.div {
+    margin: calc(100%/6);
+}
+</style>
